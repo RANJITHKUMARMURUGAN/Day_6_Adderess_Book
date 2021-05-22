1332b23a960daf87f14259f3290f10c00deccfd4
RANJITHKUMAR <ranjithm1193@gmail.com>
2021-05-22 09:21:42 +0530

    [Ranjith] modified Adderess_book.java

--- Adderess_book.java
+++ Adderess_book.java
@@ -18,6 +18,48 @@ class Address {
 		this.mobileNo = mobileNo;
 		this.email = email;
 	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getPin() {
+		return pin;
+	}
+	public void setPin(String pin) {
+		this.pin = pin;
+	}
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
 public class Adderess_Book {
 	public static void main(String[] args) {
