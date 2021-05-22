ce1f03e9974e94ab1a00e97197671860345984b1
RANJITHKUMAR <ranjithm1193@gmail.com>
2021-05-22 09:14:49 +0530

    [Ranjith] modified Adderess_book.java

--- Adderess_book.java
+++ Adderess_book.java
@@ -1,1 +1,38 @@
 import java.util.*;
+class Address {
+	private String firstName;
+	private String lastName;
+	private String city;
+	private String state;
+	private String pin;
+	private String mobileNo;
+	private String email;
+
+	AddressBook(String firstName, String lastName, String city, String state, String pin, String mobileNo,String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.city = city;
+		this.state = state;
+		this.pin = pin;
+		this.mobileNo = mobileNo;
+		this.email = email;
+	}
+}
+public class Adderess_Book {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		Address add[] = new Address[10];
+		int count = 0;
+		String firstName;
+		String lastName;
+		String city;
+		String state;
+		String pin;
+		String mobileNo;
+		String email;
+		int choice=0;
+
+	}
+}
